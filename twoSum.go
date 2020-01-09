25142b664b058e5e052d72e84675c50976b4994b
lgy-h <15638237325@163.com>
2020-01-09 09:36:43 +0800

    fucking...

---
+++ twoSum.go
@@ -0,0 +1,23 @@
+/*
+The sum of two nums
+by lgy ...
+Rookie struggle 
+*/
+package main 
+import "fmt"
+//暴力破解法
+func twoSum(nums []int, target int) []int {
+	var re []int
+	for i:=0; i < len(nums)-1;i++{
+		for j:= i + 1; j < len(nums); j++{
+			if nums[i] +nums[j] == target{
+				re = []int{i, j}
+				break
+			}
+		}
+	}
+    return re
+}
+func main(){
+	fmt.Println("gogogogo")
+}
