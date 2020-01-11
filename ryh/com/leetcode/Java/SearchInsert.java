736f3633ee1333fcb738b89fc7a1cb9e9ce4d47a
renxiaosheng21 <17839944341@163.com>
2020-01-11 20:55:02 +0800

    搜索插入位置

---
+++ ryh/com/leetcode/Java/SearchInsert.java
@@ -0,0 +1,55 @@
+
+public class SearchInsert {
+    /*给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+	如果目标值不存在于数组中，返回它将会被按顺序插入的位置
+	*/
+    //法1、暴力破解法
+    int searchInsert1(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0)
+            return 0;
+        else {
+            for (int i = 0; i < len; i++) {
+                if (nums[0] > target)//排序数组
+                    return 0;
+                else if (nums[len - 1] < target)//排序数组
+                    return len;
+                else if (nums[i] == target)
+                    return i;
+                else if (nums[i] < target && nums[i + 1] > target)
+                    return i + 1;
+            }
+        }
+        return len;
+    }
+    //法2、二分查找法
+    int searchInsert2(int[] nums,int target)
+    {
+        int len = nums.length;
+        if(len==0){
+            return 0;
+        }
+        if(nums[len-1]<target){
+            return len;
+        }
+        if (nums[0] > target)//排序数组
+            return 0;
+
+        int left = 0;
+        int right = len-1;
+        while(left<right)
+        {
+            //int mid = (left+right)/2;  left+right很可能会溢出，用下边的计算方式
+            int mid = left +(right-left)/2;
+            if(nums[mid]<target)
+            {
+                left = mid+1;//查找有半部分
+            }
+            else
+            {
+                right = mid;//查找左半部分的数据
+            }
+        }
+        return left;
+    }
+}
