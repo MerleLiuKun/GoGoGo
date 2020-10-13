24c43782ebf24c980f390d2e2da8dfb7a4491727
Ikaros <merle.liukun@gmail.com>
2020-10-13 22:55:02 +0800

    feat(solution): :art: 26 improve

--- ikaros/0026-remove-dup-from-sorted-array/remove_dup.py
+++ ikaros/0026-remove-dup-from-sorted-array/remove_dup.py
@@ -5,6 +5,7 @@ from typing import List
 
 
 class Solution:
+    # 双指针 数据交换
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
@@ -20,7 +21,22 @@ class Solution:
             c_idx += 1
         return p_idx + 1
 
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        count = 0
+        length = len(nums)
+
+        for i in range(1, length):
+            # 如果当前值与前一步相同，则 count 前进一步保持与 i 的距离
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                nums[i - count] = nums[i]
+
+        return length - count
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.removeDuplicates([1]))
+    print(s.removeDuplicates2([1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2]))
