2e6d531eeb499cc6de1f5042b3ffbcb55503302a
ikaroskun <merle.liukun@gmail.com>
2020-10-14 13:28:54 +0800

    feat(improve): :art: 104 improve

--- ikaros/0104-max-depth-binary-tree/max_depth_binary_tree.py
+++ ikaros/0104-max-depth-binary-tree/max_depth_binary_tree.py
@@ -33,6 +33,23 @@ class Solution:
             return 0
         return max(self.max_depth_simple(root.left), self.max_depth_simple(root.right)) + 1
 
+    def bfs(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        ans, arr = 0, []
+        arr.append(root)
+        while arr:
+            size = len(arr)
+            while size > 0:
+                node = arr.pop(0)
+                if node.left is not None:
+                    arr.append(node.left)
+                if node.right is not None:
+                    arr.append(node.right)
+                size -= 1
+            ans += 1
+        return ans
+
 
 if __name__ == "__main__":
     root = TreeNode(3)
@@ -45,3 +62,4 @@ if __name__ == "__main__":
     s = Solution()
     print(s.maxDepth(root))
     print(s.max_depth_simple(root))
+    print(s.bfs(root))
