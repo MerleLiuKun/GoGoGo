29f77203818dc266b6147411d6b19fc84f3040da
ikaros <merle.liukun@gmail.com>
2022-04-27 14:14:55 +0800

    feat(dp):

--- ikaros/0005-longest-palind-substring/max_substring.go
+++ ikaros/0005-longest-palind-substring/max_substring.go
@@ -1,11 +1,30 @@
+package main
+
+/*
+   Missing suoltion
+*/
 
 func longestPalindrome(s string) string {
-    n: len(s)
-    ans := ""
-    dp := make([][]int, n)
-    for i:=0; i<n; i++ {
-        dp[i] = make([]int, n)
-    }
+	if s == "" {
+		return ""
+	}
 
-    for 
+	start, end := 0, 0
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for ; left >= 0 && right < len(s) && s[left] == s[right]; left, right = left-1, right+1 {
+	}
+	return left + 1, right - 1
 }
---
+++ ikaros/0005-longest-palind-substring/max_substring.py
@@ -0,0 +1,36 @@
+"""
+最长回文字符串
+"""
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        # 如果是单个字符直接返回
+        if n < 2:
+            return s
+        
+        max_len = 1
+        begin = 0
+        # 构建结果矩阵
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        
+        for L in range(2, n + 1):
+            for i in range(n):
+                j = L +  i -1
+                if j >= n:
+                    break
+                
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                else:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
+                
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    begin = i
+        
+        return s[begin: begin + max_len]
